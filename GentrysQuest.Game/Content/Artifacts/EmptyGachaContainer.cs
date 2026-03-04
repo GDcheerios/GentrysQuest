23dbb49e76e6dfc5c754db684ccccd6d92b03044
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-03-04 02:09:05 -0600

    Added EmptyGachaContainer

---
+++ GentrysQuest.Game/Content/Artifacts/EmptyGachaContainer.cs
@@ -0,0 +1,12 @@
+using GentrysQuest.Game.Entity;
+
+namespace GentrysQuest.Game.Content.Artifacts;
+
+public class EmptyGachaContainer : Artifact
+{
+    public EmptyGachaContainer()
+    {
+        Name = "Empty Gacha Container";
+        Description = "An empty gacha container. It looks like there was something in here.";
+    }
+}
