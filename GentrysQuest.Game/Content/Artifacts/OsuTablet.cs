80a38cd2df9fdd853b9eb54ae0f1bbede3df4335
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-04-10 02:56:16 -0500

    check for null

--- GentrysQuest.Game/Content/Artifacts/OsuTablet.cs
+++ GentrysQuest.Game/Content/Artifacts/OsuTablet.cs
@@ -22,12 +22,12 @@ namespace GentrysQuest.Game.Content.Artifacts
 
         private void handleHit(DamageDetails details)
         {
-            if (Holder.Weapon != null && Holder.Weapon.AttackAmount % (10 - StarRating.Value) == 0)
-            {
-                int percent = 50 + (20 * Stack);
-                details.Damage += (int)MathBase.GetPercent(details.Damage, percent);
-                details.IsCrit = true;
-            }
+            if (details == null) return;
+            if (Holder?.Weapon == null || Holder?.Weapon.AttackAmount % (10 - StarRating.Value) != 0) return;
+
+            int percent = 50 + (20 * Stack);
+            details.Damage += (int)MathBase.GetPercent(details.Damage, percent);
+            details.IsCrit = true;
         }
 
         public OsuTablet()
