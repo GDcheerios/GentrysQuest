526b029c1740979a8e96dcb878ee0a7aabd5643b
Brayden Messerschmidt <minecreeper0913@gmail.com>
2026-04-29 00:21:48 -0500

    remove unecessary code

--- GentrysQuest.Game/Online/API/Requests/Account/RemoveItemRequest.cs
+++ GentrysQuest.Game/Online/API/Requests/Account/RemoveItemRequest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using GentrysQuest.Game.Online.API.Requests.Responses;
--- GentrysQuest.Game/Online/API/Requests/User/RefreshRankingRequest.cs
+++ GentrysQuest.Game/Online/API/Requests/User/RefreshRankingRequest.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Threading.Tasks;
 using GentrysQuest.Game.Online.API.Requests.Responses;
 
